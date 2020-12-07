a7ec465b4999bac095e18ea4723c8ec5e92253dd
Diptopol Dam <diptodam@gmail.com>
2020-12-06 21:15:50 -0500

    Added comment to explain the experiment procedure

--- src/main/java/com/paperreplication/init/App.java
+++ src/main/java/com/paperreplication/init/App.java
@@ -31,13 +31,52 @@ public class App {
     public static void main(String[] args) {
         logger.info("Start Time: {}", new Date());
 
+        /*
+         * InitialDataLoading method load the GooglePostCleanData.out file split the dataset into training and test data.
+         * We have considered the first 7 days data as training data and considered the remaining data as test data.
+         * We have stored the dataset in separate files(e.g. trainingDataSet.csv, testDataSet.csv).
+         *
+         * Since now we are no longer required to split the data, we have commented out the method.
+         */
         //initialDataLoading();
+
+        /*
+         * saveDistinctTestSuiteList method will save the unique test suites id in a file which will be used later to
+         * create the co-failure distribution matrix.
+         */
+        //saveDistinctTestSuiteList(trainingDataSet, "distinctTestSuiteList.csv");
+
+        /*
+         * Below these two consecutive methods generates the co-failure distribution matrix from the trainingData. First
+         * method will generate matrix for scenario where t1 test failed and second method will generate matrix for scenario
+         * where t1 test passed. Both generated matrix were saved using java serialization in files.
+         *
+         * We have commented out generation of matrix and fetch the saved files to fetch the matrix.
+         */
         //createProbabilityMatrix("t1FailedMatrix.dat", "FAILED");
         //createProbabilityMatrix("t1PassedMatrix.dat", "PASSED");
 
+        /*
+         * writeFifoBaselineOutput method will order the tests according to FIFOBaseline. Then the testSuites will be
+         * grouped by Change Request ID. The result map will be stored in the file using Java Serialization.
+         *
+         * Commented out the method after the output is saved in the file. TO measure the performance read the output
+         * from the file
+         */
         //writeFifoBaselineOutput();
+
+        /*
+         * writeCodyNaqSingleOutput method will perform the CodynaQSingle algorithm and re-prioritize the testSuites. Then
+         * the testSuites will be saved as grouped by Change Request ID using Java Serialization.
+         *
+         * Commented out the method after the output is saved in the file. TO measure the performance read the output
+         * from the file
+         */
         //writeCodyNaqSingleOutput();
 
+        /*
+         * evaluatePerformance method will measure the performance of the experiment
+         */
         evaluatePerformance();
     }
 
@@ -68,31 +107,6 @@ public class App {
 
         double medianPercentageDelayAtLeastOneFailure = PerformanceEvaluationUtils.getPercentageDelayWithAtLeastOneFailure(fifoResult, codynaqSingleResult);
         logger.info("Percentage Delay  At Least One Failure: {}", medianPercentageDelayAtLeastOneFailure);
-    }
-
-    private static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
-        Set<Object> seen = ConcurrentHashMap.newKeySet();
-        return t -> seen.add(keyExtractor.apply(t));
-    }
-
-    private static List<Integer> getAllPassedChangeRequestIdList() {
-        String preDataSetFileName = "GooglePostCleanData.out";
-        List<DataSet> preDataSetList = Utils.getDataSetList(preDataSetFileName, false);
-
-        System.out.println(preDataSetList.stream().filter(distinctByKey(DataSet::getChangeRequestId)).count());
-
-        Map<Integer, List<DataSet>> map = preDataSetList.stream().collect(Collectors.groupingBy(DataSet::getChangeRequestId));
-
-        List<Integer> allPassedChangeRequestIdList = new ArrayList<>();
-        for (Map.Entry<Integer, List<DataSet>> entry: map.entrySet()) {
-            List<DataSet> dataSetList = entry.getValue();
-
-            if (dataSetList.stream().allMatch(dataSet -> dataSet.getStatus().equals("PASSED"))) {
-                allPassedChangeRequestIdList.add(entry.getKey());
-            }
-        }
-
-        return allPassedChangeRequestIdList;
     }
 
     private static void writeFifoBaselineOutput(){
@@ -131,8 +145,6 @@ public class App {
     private static Map<Integer, List<DataSet>> performCodynaqSingleParallel(List<DataSet> testDataSet) {
         Map<Integer, List<DataSet>> testDataSetGroupByChangeRequestId = testDataSet.stream()
                 .collect(Collectors.groupingBy(DataSet::getChangeRequestId));
-
-        //Map<Integer, List<DataSet>> testDataSetGroupByChangeRequestId2 = Collections.singletonMap(4097, testDataSetGroupByChangeRequestId.get(4097));
 
         Map<Integer, List<DataSet>> finishedTestSetGroupByChangeRequestId = new ConcurrentHashMap<>();
 
@@ -233,8 +245,6 @@ public class App {
 
     private static void createProbabilityMatrix(String fileName, String status) {
         List<DataSet> trainingDataSet = Utils.getDataSetList("trainingDataSet.csv", true);
-
-        //List<String> distinctTestSuiteList = getDistinctTestSuiteListAfterSerialization(trainingDataSet, "distinctTestSuiteList.csv");
         List<String> distinctTestSuiteList = Utils.getDistinctTestSuiteList("distinctTestSuiteList.csv");
 
         int size = distinctTestSuiteList.size();
@@ -265,7 +275,7 @@ public class App {
         logger.info("Write completed");
     }
 
-    private static List<String> getDistinctTestSuiteListAfterSerialization(List<DataSet> dataSetList, String fileName) {
+    private static void saveDistinctTestSuiteList(List<DataSet> dataSetList, String fileName) {
         List<String> distinctTestSuiteList = dataSetList.stream()
                 .map(DataSet::getTestSuite)
                 .distinct()
@@ -273,8 +283,6 @@ public class App {
                 .collect(Collectors.toList());
 
         Utils.writeDistinctTestSuiteList(distinctTestSuiteList, fileName);
-
-        return distinctTestSuiteList;
     }
 
     private static void initialDataLoading() {
